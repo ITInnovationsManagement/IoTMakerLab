1265bb98a4bd3ae3f988891a100d3593084af5d5
ITIMZF <itim@zf.com>
2017-02-20 20:17:53 +0100

    Update Test_LED.py

--- Fallbeispiel 1/Test_LED.py
+++ Fallbeispiel 1/Test_LED.py
@@ -1,18 +1,33 @@
-import RPi.GPIO as GPIO  
+import RPi.GPIO as GPIO
 import time
+                                                      
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-# blinking function  
-def blink(pin):  
-        GPIO.output(pin,GPIO.HIGH)  
-        time.sleep(1)  
-        GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)  
-        return  
-# to use Raspberry Pi board pin numbers  
-GPIO.setmode(GPIO.BOARD)  
-# set up GPIO output channel  
-GPIO.setup(11, GPIO.OUT)  
-# blink GPIO17 50 times  
-for i in range(0,50):  
-        blink(11)  
-GPIO.cleanup()
+#Setzt PIN12 als Ausgang
+GPIO.setup(12, GPIO.OUT)
+print("LED ROT AN")
+#Legt Strom an PIN12 an
+GPIO.output(12,GPIO.HIGH)
+time.sleep(2)
+#Entfernt Strom von PIN12
+GPIO.output(12, GPIO.LOW)
+
+#Setzt PIN13 als Ausgang
+GPIO.setup(13, GPIO.OUT)
+print("LED GELB AN")
+#Legt Strom an PIN13 an
+GPIO.output(13,GPIO.HIGH)
+time.sleep(1)
+#Entfernt Strom von PIN15
+GPIO.output(13, GPIO.LOW)
+
+#Setzt PIN15 als Ausgang
+GPIO.setup(15, GPIO.OUT)
+print("LED GRÜN AN")
+#Legt Strom an PIN15 an
+GPIO.output(15,GPIO.HIGH)
+time.sleep(1)
+#Entfernt Strom von PIN15
+GPIO.output(15, GPIO.LOW)
+
